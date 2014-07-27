3f7195d189f17ae7d85bba929e2c24e09dd30451
David Smith <ds423769@gmail.com>
2014-07-27 12:03:31 -0400

    Fixed the scraper. The webview needs some work because it is not showing the site's content

--- source/src/com/android/projecte/townportal/NewsActivity.java
+++ source/src/com/android/projecte/townportal/NewsActivity.java
@@ -5,20 +5,16 @@
 package com.android.projecte.townportal;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.net.URLDecoder;
 import java.util.List;
 import java.util.Vector;
 
-import org.apache.http.NameValuePair;
-import org.apache.http.client.utils.URLEncodedUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 /*
  * News Activity
@@ -37,9 +33,6 @@ final public class NewsActivity extends FeedActivity {
         this.newsSource = getString( R.string.newsSource );
         this.title = getString( R.string.news_text );
         this.seeMoreUrl = getString( R.string.newsViewMore );
-        
-        // Set title and courtesy
-
         this.courtesyText.setText( getString( R.string.newsCourtesy ) );
         
         new FeedTask( this.context ).execute();
@@ -56,44 +49,35 @@ final public class NewsActivity extends FeedActivity {
             Document htmlDoc = Jsoup.connect( String.format( newsSource + "default.aspx?section=%s", 
                     getString( R.string.topNews ) ) ).get();
             
-            Elements newsItems = htmlDoc.select( "li[data-icon]" );
+            Elements newsItems = htmlDoc.select( "a[class=list-group-item]" );
             
             // Iterate through all items
             for ( Element element : newsItems ) {
                 
-                // Used http://jsoup.org/cookbook/ to figure out how to parse
-                String title = element.select( "div[id=storySummary] h1, h2, h3, h4, h5, h6" ).get( 0 ).text();
-                String description = element.select( "div[id=storySummary] p" ).get( 0 ).text();
-                String link = null;
-                
-                // Get true link
-                List<NameValuePair> uriPairs = URLEncodedUtils.parse( 
-                        new URI( newsSource + element.select( "a" ).get( 0 ).attr( "href" ) ), "UTF-8" );
-                
-                // Find link within pairs
-                for ( NameValuePair nvp : uriPairs ) {
-                    
-                    if ( nvp.getName().equalsIgnoreCase( "link" ) ) {
-                        
-                        link = nvp.getValue();
-                        break;
-                    }
-                }
+                // http://jsoup.org/apidocs/org/jsoup/select/Selector.html
+                String title       = element.select( ".articleTitle" ).get( 0 ).text();
+                String description = "test-description";
+                String link        = null;
+                String lousy_link  = element.select( "a" ).get( 0 ).attr( "href" );
 
+                if(lousy_link != null)
+                {
+                	// lousy_link is sometimes only partially encoded. Making it uniform.
+                	link = URLDecoder.decode(newsSource + lousy_link, "UTF-8");
+                }
+                
                 // Add the item if applicable
                 if ( title != null && link != null && description != null )
+                {
                     result.add( new Item( title, description, link ) );
+                }
             }
             
         } catch ( IOException e ) {
             
             e.printStackTrace();
             
-        } catch ( URISyntaxException e ) {
-            
-            e.printStackTrace();
         }
-
         return result;
     }
     
